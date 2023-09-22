f456a4e59875c0c94ee2a86494d8759c307ed85e
LucasGervasi <you@example.com>
2023-09-22 13:26:11 -0300

    razonsocial new deploy

--- server/server.js
+++ server/server.js
@@ -5,29 +5,21 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const fs = require('fs');
-const soap = require('soap');
-
 
 const db = mysql.createPool({
-  database: "railway",
-  user: "root",
-  host: "containers-us-west-190.railway.app",
-  port: "7782",
-  password: "fpBGPVooo4V5ad1TxiCs",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
 });
-
-//const MYSQL_URL = 'mysql://root:fpBGPVooo4V5ad1TxiCs@containers-us-west-190.railway.app:7782/railway',
 
 app.get("/", (req, res) => {
   return res.status(200).send("Backend Conectado");
 });
 
-
-app.post("/data", (req, res) => {
-  const impoCompraVenta = req.body.impoCompraVenta;
-  const archivoData = req.body.archivo;
-
+// Función para insertar archivo en la base de datos
+const insertArchivo = (archivoData) => {
   const {
     idusuario,
     idempresa,
@@ -37,61 +29,78 @@ app.post("/data", (req, res) => {
     fechadesde,
     fechaupload,
   } = archivoData;
-  const sqlArchivo = `INSERT INTO archivo ( idusuario, idempresa, archivo, tipo, fechahasta, fechadesde, fechaupload) VALUES ( ${idusuario}, ${idempresa}, '${archivo}', '${tipo}', '${fechahasta}', '${fechadesde}', '${fechaupload}')`;
+  const sqlArchivo = `INSERT INTO archivo ( idusuario, idempresa, archivo, tipo, fechahasta, fechadesde, fechaupload) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  const values = [idusuario, idempresa, archivo, tipo, fechahasta, fechadesde, fechaupload];
 
-  db.query(sqlArchivo, (err, resultArchivo) => {
-    if (err) {
-      console.error("Error al insertar archivo en la base de datos:", err);
-      return res.status(500).send("Error interno del servidor");
-    }
+  return new Promise((resolve, reject) => {
+    db.query(sqlArchivo, values, (err, resultArchivo) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(resultArchivo.insertId);
+      }
+    });
+  });
+};
 
-    const idarchivo = resultArchivo.insertId;
+// Función para insertar impoCompraVenta en la base de datos
+const insertImpoCompraVenta = (idarchivo, data1) => {
+  const {
+    fecha,
+    tipoCFE,
+    serie,
+    numero,
+    RUTEmisor,
+    moneda,
+    montoneto,
+    montoiva,
+    montototal,
+    montoretper,
+    montocredFiscal,
+  } = data1;
+  const sqlImpo = `INSERT INTO impo_compraventa ( idarchivo, fecha, tipoCFE, serie, numero, RUTEmisor, moneda, montoneto, montoiva, montototal, montoretper, montocredFiscal) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [idarchivo, fecha, tipoCFE, serie, numero, RUTEmisor, moneda, montoneto, montoiva, montototal, montoretper, montocredFiscal || 0];
+
+  return new Promise((resolve, reject) => {
+    db.query(sqlImpo, values, (err, resultImpo) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log("Dato insertado correctamente en la posición");
+        resolve();
+      }
+    });
+  });
+};
+
+app.post("/data", async (req, res) => {
+  try {
+    const impoCompraVenta = req.body.impoCompraVenta;
+    const archivoData = req.body.archivo;
+
+    const idarchivo = await insertArchivo(archivoData);
 
     const insertionErrors = [];
 
-    impoCompraVenta.forEach((data1, index) => {
-      const {
-        fecha,
-        tipoCFE,
-        serie,
-        numero,
-        RUTEmisor,
-        moneda,
-        montoneto,
-        montoiva,
-        montototal,
-        montoretper,
-        montocredFiscal,
-      } = data1;
-      console.log(fecha, "ACA ESTA LA FECHA");
-      const sqlImpo = `INSERT INTO impo_compraventa ( idarchivo, fecha, tipoCFE, serie, numero, RUTEmisor, moneda, montoneto, montoiva, montototal, montoretper, montocredFiscal) VALUES ( ${idarchivo}, '${fecha}', '${tipoCFE}', '${serie}', ${numero}, '${RUTEmisor}', '${moneda}', ${montoneto}, ${montoiva}, ${montototal}, ${montoretper}, ${
-        montocredFiscal || 0
-      })`;
+    for (const data1 of impoCompraVenta) {
+      try {
+        await insertImpoCompraVenta(idarchivo, data1);
+      } catch (err) {
+        console.error("Error al insertar impo_compraventa en la base de datos:", err);
+        insertionErrors.push(err);
+      }
+    }
 
-      db.query(sqlImpo, (err, resultImpo) => {
-        if (err) {
-          console.error(
-            "Error al insertar impo_compraventa en la base de datos:",
-            err
-          );
-          insertionErrors.push(err);
-        } else {
-          console.log("Dato insertado correctamente en la posición");
-        }
-
-        if (index === impoCompraVenta.length - 1) {
-          if (insertionErrors.length > 0) {
-            return res.status(500).send("Error interno del servidor");
-          } else {
-            console.log("Todos los datos insertados correctamente");
-            return res
-              .status(200)
-              .send("Todos los datos insertados correctamente");
-            }
-          }
-      });
-    });
-  });
+    if (insertionErrors.length > 0) {
+      return res.status(500).send("Error interno del servidor");
+    } else {
+      console.log("Todos los datos insertados correctamente");
+      return res.status(200).send("Todos los datos insertados correctamente");
+    }
+  } catch (error) {
+    console.error("Error en POST /data:", error);
+    return res.status(500).send("Error interno del servidor");
+  }
 });
 
   // Pedir datos de moneda_cotizacion
@@ -190,38 +199,38 @@ app.post('/razonsocial', (req, res) => {
   
     
 
-//   app.delete("/data", (req, res) => {
-//     let operationsCompleted = 0;
+  app.delete("/data", (req, res) => {
+    let operationsCompleted = 0;
     
-//   // Eliminar datos de impo_compraventa
-//   db.query("DELETE FROM impo_compraventa", (err, result) => {
-//     if (err) {
-//       console.error("Error al eliminar datos previos:", err);
-//       return res.status(500).send("Error interno del servidor");
-//     }
-//     console.log("Datos de impo_compraventa eliminados correctamente");
-//     operationsCompleted++;
-//     checkAllOperationsCompleted();
-//   });
+  // Eliminar datos de impo_compraventa
+  db.query("DELETE FROM impo_compraventa", (err, result) => {
+    if (err) {
+      console.error("Error al eliminar datos previos:", err);
+      return res.status(500).send("Error interno del servidor");
+    }
+    console.log("Datos de impo_compraventa eliminados correctamente");
+    operationsCompleted++;
+    checkAllOperationsCompleted();
+  });
 
-//   // Eliminar datos de archivo
-//   db.query("DELETE FROM archivo", (err, result) => {
-//     if (err) {
-//       console.error("Error al eliminar datos previos:", err);
-//       return res.status(500).send("Error interno del servidor");
-//     }
-//     console.log("Datos de archivo eliminados correctamente");
-//     operationsCompleted++;
-//     checkAllOperationsCompleted();
-//   });
+  // Eliminar datos de archivo
+  db.query("DELETE FROM archivo", (err, result) => {
+    if (err) {
+      console.error("Error al eliminar datos previos:", err);
+      return res.status(500).send("Error interno del servidor");
+    }
+    console.log("Datos de archivo eliminados correctamente");
+    operationsCompleted++;
+    checkAllOperationsCompleted();
+  });
 
-//   function checkAllOperationsCompleted() {
-//     if (operationsCompleted === 3) {
-//       // Ajusta el número total de operaciones
-//       return res.send("Operaciones completadas correctamente");
-//     }
-//   }
-// });
+  function checkAllOperationsCompleted() {
+    if (operationsCompleted === 3) {
+      // Ajusta el número total de operaciones
+      return res.send("Operaciones completadas correctamente");
+    }
+  }
+});
 
 
 
