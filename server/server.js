58ea90acda4f81555bca28fb4d72b0966881f973
LucasRomanGervasi <gervasilucas22@gmail.com>
2023-10-20 19:24:15 -0300

    ErrorSintaxis

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ app.post("/data", (req, res) => {
       } = data1;
       console.log(data1['fecha'], "ACA ESTA LA FECHA");
       const sqlImpo = `INSERT INTO impo_compraventa ( idarchivo, fecha, tipoCFE, serie, numero, RUTEmisor, moneda, montoNetoUYU, montoneto,monoIvaUYU, montoiva, montototal, montoRetPerUYU, montoretper, montoCredFiscalUYU, montocredFiscal, razonsocial, domicilio) VALUES ( ${idarchivo}, '${fecha}', '${tipoCFE}', '${serie}', ${numero}, '${RUTEmisor}', '${moneda}', ${montonetoUYU}, ${montoneto}, ${montoivaUYU}, ${montoiva}, ${montototal}, ${montoretperUYU}, ${montoretper},  
-       ${montocredfiscalUYU || 0} ${montocredfiscal || 0}, '${razonsocial}', '${domicilio}')`;
+       ${montocredfiscalUYU || 0}, ${montocredfiscal || 0}, '${razonsocial}', '${domicilio}')`;
 
       db.query(sqlImpo, (err, resultImpo) => {
         if (err) {
